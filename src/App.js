c7df8569702f15114570301674654da020fbbf74
sofyan warkay <117473315+warkaysofyan@users.noreply.github.com>
2024-01-10 01:08:49 +0100

    adding the tic tac teo game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import './App.css'
+//import './App.css'
+
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from 'react-router-dom'
+import RootLayout from './pages/layouts/RootLayout'
+import Tic from './pages/games/Tic'
 
 function App() {
-	return <h1>hello, world</h1>
+	let router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path='/' element={<RootLayout />}>
+				<Route index element={<Tic />} />
+			</Route>
+		)
+	)
+	return <RouterProvider router={router} />
 }
 
 export default App
